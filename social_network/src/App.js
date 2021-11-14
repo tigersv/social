01862ed699bc807bf55362ce522d3cd547977999
NikTikJK <nsahov700@gmail.com>
2021-11-14 19:43:37 +0300

    Delete all from App.js

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -4,30 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className="spinner-border text-primary" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-secondary" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-success" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-danger" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-warning" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-info" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-light" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-border text-dark" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
+        App
     </div>
   );
 }
